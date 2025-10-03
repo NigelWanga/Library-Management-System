e05e85b5bfc3425afe7c65791923b6a5964a5fb6
NigelWanga <nigelklose02@gmail.com>
2025-10-02 23:09:35 -0400

    R-TEST-02
    RESP_02_test_01
    RESP_02_test_02

--- src/test/java/org/example/LibraryTest.java
+++ src/test/java/org/example/LibraryTest.java
@@ -21,12 +21,37 @@ public class LibraryTest {
     @Test
     @DisplayName("Check library catalogue for valid book - Great Gatsby.")
     void RESP_01_test_02(){
-
         InitializeLibrary library = new InitializeLibrary();
         Catalogue catalogue = library.initializeLibrary();
 
         Book book = catalogue.getBook(0);
         String title = book.getTitle();
         assertEquals("Great Gatsby",title);
+    }
+
+    @Test
+    @DisplayName("Check borrower accounts size is 3")
+    void RESP_02_test_01(){
+        BorrowerRegistry borrowerRegistry = new BorrowerRegistry();
+        borrowerRegistry.initializeBorrowers();
+
+        int borrowSize = borrowerRegistry.getBorrowerSize();
+
+        assertEquals(3, borrowSize);
+
+    }
+
+    @Test
+    @DisplayName("Check initial books borrowed is 0")
+    void RESP_02_test_02(){
+        BorrowerRegistry borrowerRegistry = new BorrowerRegistry();
+        borrowerRegistry.initializeBorrowers();
+
+
+        for (Borrower borrower : borrowerRegistry.getAllBorrowers()){
+            int borrowBookSize = borrower.getBorrowedBooksSize();
+            assertEquals(0, borrowBookSize);
+        }
+
     }
 }
